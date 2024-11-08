ad6611570e7eaddeea005088d44ceb1fb60b6112
google authorization <harsh.kumar@triconinfotech.com>
2024-11-08 11:22:19 +0530

    updated mysql conn

--- database/connection.go
+++ database/connection.go
@@ -1,21 +1,29 @@
 package database
 
 import (
+	"log"
+
 	"github.com/harshtricon/notes-backend-main/models"
-	"gorm.io/driver/sqlite"
+	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
 
 func Connect() {
-	connection, err := gorm.Open(sqlite.Open("Note.db"), &gorm.Config{})
+	dsn := "user:admin@tcp(127.0.0.1:3306)/mynotes?charset=utf8mb4&parseTime=True&loc=Local"
+	connection, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
-		panic("could not connect to the database")
+		log.Fatalf("could not connect to the database: %v", err)
 	}
 
 	DB = connection
 
-	connection.AutoMigrate(&models.User{}, &models.Note{})
+	// Automatically migrate your schema, keep this for initial setup
+	if err := connection.AutoMigrate(&models.User{}, &models.Note{}); err != nil {
+		log.Fatalf("could not migrate database schema: %v", err)
+	}
+
+	log.Println("Connected to MySQL database successfully.")
 }
--- go.mod
+++ go.mod
@@ -3,9 +3,16 @@ module github.com/harshtricon/notes-backend-main
 go 1.23.2
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/gofiber/fiber/v2 v2.52.5
+	golang.org/x/crypto v0.14.0
+	gorm.io/driver/sqlite v1.5.6
+	gorm.io/gorm v1.25.12
+)
+
+require (
 	github.com/andybalholm/brotli v1.0.5 // indirect
-	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
-	github.com/gofiber/fiber/v2 v2.52.5 // indirect
+	github.com/go-sql-driver/mysql v1.7.0 // indirect
 	github.com/google/uuid v1.5.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
@@ -20,6 +27,5 @@ require (
 	github.com/valyala/tcplisten v1.0.0 // indirect
 	golang.org/x/sys v0.15.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
-	gorm.io/driver/sqlite v1.5.6 // indirect
-	gorm.io/gorm v1.25.12 // indirect
+	gorm.io/driver/mysql v1.5.7 // indirect
 )
