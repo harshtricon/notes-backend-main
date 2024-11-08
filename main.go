4ae642c763416c54cce7b88d80a60d54623e7bef
google authorization <harsh.kumar@triconinfotech.com>
2024-11-08 12:46:39 +0530

    removed frontend con forr now

--- main.go
+++ main.go
@@ -1,22 +1,17 @@
 package main
 
 import (
+	"log"
+
 	"github.com/harshtricon/notes-backend-main/database"
 	"github.com/harshtricon/notes-backend-main/routes"
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
 	database.Connect()
-
 	app := fiber.New()
-
-	app.Use(cors.New(cors.Config{
-		AllowCredentials: true,
-	}))
-
 	routes.Setup(app)
-
-	app.Listen(":8000")
+	log.Println("Server is running at http://localhost:8000")
+	log.Fatal(app.Listen(":8000"))
 }
